11cc8895b192864a01dfea3b8a772c47c11351b7
rph <rhansen0127@gmail.com>
2024-08-25 23:31:46 -0500

    added reset css, a basic color to root of styles and finished rough post logic

---
+++ public/js/single-post.js
@@ -0,0 +1,32 @@
+const commentBtn = document.querySelector("#add-comment");
+const commentBox = document.querySelector("#comment-box");
+const form = document.querySelector("#comment-form");
+const submitBtn = document.querySelector("#comment-submit-btn");
+
+const addComment = () => {
+  console.log("clicked");
+  commentBox.classList.toggle("d-none");
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const newComment = {
+    body: e.target.elements.comment.value,
+    post_id: submitBtn.dataset.postId,
+  };
+  await fetch("/api/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newComment),
+  }).then((res) => {
+    if (res.status === 201) {
+      location.reload();
+    }
+  });
+};
+
+commentBtn.addEventListener("click", addComment);
+form.addEventListener("submit", handleSubmit);
